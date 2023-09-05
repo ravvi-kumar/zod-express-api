157cd733ff09ecd5ef9b39027c98af4024caa706
ravvi-kumar <ravvi.kumar8989@gmail.com>
2023-09-05 23:15:19 +0530

    add todo ( w/out request validator middleware )

--- src/api/todos/todos.controller.ts
+++ src/api/todos/todos.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { TodoWithId, Todos } from "./todos.model";
+import { ZodError } from "zod";
+import { Todo, TodoWithId, Todos } from "./todos.model";
 
 export async function findAll(
   req: Request,
@@ -11,6 +12,29 @@ export async function findAll(
     const todos = await result.toArray();
     res.json(todos);
   } catch (error) {
+    next(error);
+  }
+}
+export async function createOne(
+  req: Request<{}, TodoWithId, Todo>,
+  res: Response<TodoWithId>,
+  next: NextFunction
+) {
+  try {
+    const validatedResult = await Todo.parseAsync(req.body);
+    const todo = await Todos.insertOne(validatedResult);
+    if (!todo.acknowledged) {
+      throw new Error("Insertion failed");
+    }
+    res.status(201);
+    res.json({
+      _id: todo.insertedId,
+      ...validatedResult,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(422);
+    }
     next(error);
   }
 }
--- src/api/todos/todos.route.ts
+++ src/api/todos/todos.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { findAll } from "./todos.controller";
+import { findAll, createOne } from "./todos.controller";
 
 const router = Router();
 
 router.get("/", findAll);
+router.post("/", createOne);
 export default router;
